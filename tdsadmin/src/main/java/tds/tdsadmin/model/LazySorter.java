58326e03075afc7cc43c3dc0c801833a8f90d864
shajib <mkhan@air.org>
2016-02-12 13:26:51 -0500

    Implemented column sorting

--- tdsadmin/src/main/java/tds/tdsadmin/model/LazySorter.java
+++ tdsadmin/src/main/java/tds/tdsadmin/model/LazySorter.java
@@ -1,7 +1,10 @@
 package tds.tdsadmin.model;
 
 import java.util.Comparator;
+import java.util.Date;
+
 import org.primefaces.model.SortOrder;
+import org.springframework.security.util.FieldUtils;
 
 public class LazySorter implements Comparator<TestOpportunity> {
 
@@ -16,10 +19,19 @@ public class LazySorter implements Comparator<TestOpportunity> {
 
 	public int compare(TestOpportunity TestOpportunity1, TestOpportunity TestOpportunity2) {
 		try {
-			Object value1 = TestOpportunity.class.getField(this.sortField).get(TestOpportunity1);
-			Object value2 = TestOpportunity.class.getField(this.sortField).get(TestOpportunity2);
+			
+			Object value1 = FieldUtils.getFieldValue(TestOpportunity1, sortField);//TestOpportunity.class.getField(this.sortField).get(TestOpportunity1);
+			Object value2 = FieldUtils.getFieldValue(TestOpportunity2, sortField);//TestOpportunity.class.getField(this.sortField).get(TestOpportunity2);
 
-			int value = ((Comparable) value1).compareTo(value2);
+			int value;
+			if (value1 == null && value2 == null)
+				value = 0;
+			else if (value1 == null)
+				value = -1;
+			else if (value2 == null)
+				value = 1;
+			else
+				value = ((Comparable) value1).compareTo(value2);
 
 			return SortOrder.ASCENDING.equals(sortOrder) ? value : -1 * value;
 		} catch (Exception e) {
