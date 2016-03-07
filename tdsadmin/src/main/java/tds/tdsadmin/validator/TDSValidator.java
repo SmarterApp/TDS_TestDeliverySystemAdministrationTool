383bcba611fe2882d447599398b26e8c5c21065c
shajib <mkhan@air.org>
2016-03-07 18:13:48 -0500

    Integration of progman and mna

--- tdsadmin/src/main/java/tds/tdsadmin/validator/TDSValidator.java
+++ tdsadmin/src/main/java/tds/tdsadmin/validator/TDSValidator.java
@@ -13,7 +13,7 @@ import javax.faces.validator.ValidatorException;
 @FacesValidator("tdsValidator")
 public class TDSValidator implements Validator {
 
-	private static final String INPUT_PATTERN = "[A-Za-z0-9\\-]+";
+	private static final String INPUT_PATTERN = "[A-Za-z0-9\\-]*";
 
 	private Pattern pattern;
 	private Matcher matcher;
