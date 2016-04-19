1979ac957748e23e8ba6971398173a16586aa01f
Shajib Khan <mkhan@air.org>
2016-04-19 15:53:29 +0000

    Created new branch SB-1459-TimeTooltip

---
+++ tdsadmin/src/main/java/tds/tdsadmin/model/OpportunitySerializable.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Educational Online Test Delivery System
+ * Copyright (c) 2016 American Institutes for Research
+ * 
+ * Distributed under the AIR Open Source License, Version 1.0
+ * See accompanying file AIR-License-1_0.txt or at 
+ * http://www.smarterapp.org/documents/American_Institutes_for_Research_Open_Source_Software_License.pdf
+ ******************************************************************************/
+package tds.tdsadmin.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class OpportunitySerializable extends ArrayList<TestOpportunity> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
